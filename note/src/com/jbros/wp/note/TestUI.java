2245d0b4a243acd4219b9753ded47df3aae5a30c
jean huang <jean@jeanbrothers.com>
2011-12-18 18:08:44 +0800

    initiate

---
+++ note/src/com/jbros/wp/note/TestUI.java
@@ -0,0 +1,13 @@
+package com.jbros.rim.note;
+
+public class TestUI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.console().printf("Hello, %s, welcome to rim dev world", "Jean Huang");
+	}
+
+}
