8a6bf2225966ce1c398df10e140c1a4b99f20574
Jean Huang <beyonda520@gmail.com>
2011-12-18 20:23:55 +0800

    add android resources

---
+++ note/src/com/jbros/android/note/resource/R.java
@@ -0,0 +1,13 @@
+package com.jbros.android.note.resource;
+
+public class R {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
